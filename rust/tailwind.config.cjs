e927d1cbbb55b58fa5aa03ed1609c90bd1d3edc7
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-12-31 13:30:27 -0500

    begin adding tailwind

---
+++ rust/tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./templates/*.html'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...fontFamily.sans],
+            },
+        },
+    },
+};
